f8b69decf81b72968f1e24b7362ecd80a8f20871
muhrizkiakbar <muhrizkiakbar@live.com>
2025-01-30 10:03:27 +0800

    a

--- app/Outputs/Admin/DutyOutput.php
+++ app/Outputs/Admin/DutyOutput.php
@@ -51,7 +51,7 @@ class DutyOutput extends ApiOutput
             'state' => $object->state,
             'foto' => $object->foto ? asset('storage/'.$object->foto) : null,
             'surat_tugas' => $object->surat_tugas ? asset('storage/'.$object->surat_tugas) : null,
-            'slug' => encrypt($object->id)
+            'slug' => encrypt($object->id),
             'user_petugas' => $user_output->renderJson($object->user_petugas ?? [], "format", [ "mode" => "raw_data"]) ?? [],
             'user_admin' => $user_output->renderJson($object->user_admin ?? [], "format", [ "mode" => "raw_data" ]) ?? [],
         ];
