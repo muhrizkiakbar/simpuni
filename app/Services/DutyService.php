95f452bcac16ef9f467004711caa45a94f6ba46d
muhrizkiakbar <muhrizkiakbar@live.com>
2025-02-02 01:52:40 +0800

    a

--- app/Services/DutyService.php
+++ app/Services/DutyService.php
@@ -87,7 +87,7 @@ class DutyService extends ApplicationService
         $procject_id = 'simpuni-banjarbaru';
         $fcm = $user->fcm_token;
 
-        $credentialsFilePath = storage_path('storage/app/public/app/json/google-services.json');
+        $credentialsFilePath = storage_path('app/public/app/json/google-services.json');
         $client = new GoogleClient();
         $client->setAuthConfig($credentialsFilePath);
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
