6b5f9cb1066a1f430bbca0dcedade4c3525743da
muhrizkiakbar <muhrizkiakbar@live.com>
2025-02-24 13:37:21 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\Petugas\DutyController as PetugasDutyController;
 
 Route::get('/public/{path_file}/{file}', function ($path_file, $file, Request $request) {
     $path = storage_path('/app/public/'.$path_file.'/'.$file);
-    return response()->json([], 200);
+    return response()->json([], 200)->where('filename', '.*');
 
     return response()->stream(function () use ($path) {
         readfile($path);
